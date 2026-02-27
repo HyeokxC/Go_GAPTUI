51d42e45de316a97c5cfcdb06928168229459322
HyeokxC <jhky63@gmail.com>
2026-02-27 17:03:55 +0900

    fix: remove stale ExchangeButtons import from widgets __init__

--- tui_textual/src/kimchi_tui/widgets/__init__.py
+++ tui_textual/src/kimchi_tui/widgets/__init__.py
@@ -1,7 +1,6 @@
 from .card_view import CardView
 from .coin_table import CoinTable
 from .detail_panel import CoinDetailPanel
-from .exchange_buttons import ExchangeButtons
 from .filter_bar import FilterBar
 from .header_bar import KimchiHeader
 from .scenario_panel import ScenarioPanel
@@ -11,7 +10,6 @@ __all__ = [
     "CardView",
     "CoinDetailPanel",
     "CoinTable",
-    "ExchangeButtons",
     "FilterBar",
     "KimchiHeader",
     "KimchiStatusBar",
